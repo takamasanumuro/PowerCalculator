97596646e9d3fb79f9e85613684b897cc1e9ed86
adrianowerneck <adrianowerneck@id.uff.br>
2025-01-11 14:34:10 -0300

    Improved measurement time and state changes

--- src/client.py
+++ src/client.py
@@ -43,6 +43,7 @@ class ITech6018Device(DataSource, StateManager):
             self.send_command('SYSTEM:REMOTE\n')
             self.send_command('*CLS\n')
             self.send_command('FUNCTION:MODE FIXED\n')
+            self.send_command('SENSE:ACQUIRE:POINTS 10\n')
         except socket.error as e:
             print(f"Error connecting to device: {e}")
             self.socket.close()
@@ -88,13 +89,13 @@ class ITech6018Device(DataSource, StateManager):
         try:
             if state == PowerStates.CHARGE:
                 self.send_command("FUNCTION:MODE FIXED\n")
-                time.sleep(2)
+                time.sleep(5)
                 self.send_command("BATTERY:MODE CHARGE\n")
                 self._send_charge_rates(charge_current = current, charge_cutoff_voltage = cutoff_voltage, charge_cutoff_current = cutoff_current)
                 self.send_command("FUNCTION:MODE BATTERY\n")
             elif state == PowerStates.DISCHARGE:
                 self.send_command("FUNCTION:MODE FIXED\n")
-                time.sleep(2)
+                time.sleep(5)
                 self.send_command("BATTERY:MODE DISCHARGE\n")
                 self._send_discharge_rates(discharge_current = current, discharge_cutoff_voltage = cutoff_voltage, discharge_cutoff_current = cutoff_current)
                 self.send_command("FUNCTION:MODE BATTERY\n")
@@ -117,9 +118,9 @@ class ITech6018Device(DataSource, StateManager):
     def _send_charge_rates(self, charge_current : float, charge_cutoff_voltage : float, charge_cutoff_current : float):
         try:
             self.send_command(f"BATTERY:CHARGE:CURRENT {charge_current}\n")
-            self.send_command(f"BATTERY:CHARGE:VOLTAGE {charge_cutoff_voltage}\n") #For charge, charge voltage must be the same as the cutoff voltage
-            self.send_command(f"BATTERY:SHUT:CURRENT {charge_cutoff_current - 0.150}\n")
-            self.send_command(f"BATTERY:SHUT:VOLTAGE {charge_cutoff_voltage + 0.150}\n") #For charge, shutdown voltage must be above the charge voltage
+            self.send_command(f"BATTERY:CHARGE:VOLTAGE {charge_cutoff_voltage + 0.020}\n") #For charge, charge voltage must be the same as the cutoff voltage
+            self.send_command(f"BATTERY:SHUT:CURRENT 0\n")
+            self.send_command(f"BATTERY:SHUT:VOLTAGE {charge_cutoff_voltage + 1.000}\n") #For charge, shutdown voltage must be above the charge voltage
         except socket.error as e:
             print(f"Error setting charge rates: {e}")
 
@@ -127,7 +128,7 @@ class ITech6018Device(DataSource, StateManager):
         try:
             self.send_command(f"BATTERY:DISCHARGE:CURRENT {discharge_current}\n")
             self.send_command(f"BATTERY:DISCHARGE:VOLTAGE {discharge_cutoff_voltage - 0.150}\n") #For discharge, discharge voltage must be below the shutdown voltage
-            self.send_command(f"BATTERY:SHUT:CURRENT {discharge_cutoff_current + 0.150}\n")
+            self.send_command(f"BATTERY:SHUT:CURRENT 0\n")
             self.send_command(f"BATTERY:SHUT:VOLTAGE {discharge_cutoff_voltage - 0.150}\n") #For discharge, shutdown voltage is the same as the cutoff voltage
         except socket.error as e:
             print(f"Error setting discharge rates: {e}")
@@ -210,16 +211,28 @@ class ChargeController:
         self.state_manager.set_state(PowerStates.PASSIVE, 0.0, 0.0, 0.0)
         print("Sequence completed")
 
+def benchmark_response_time(ip: str, port: int):
+    device = ITech6018Device(ip, port)
+    time_list = []
+    for i in range(10):
+        time_begin = time.time()
+        data_point = device.read_measurements()
+        time_end = time.time()
+        time_list.append(time_end - time_begin)
+    
+    print(f"Average response time: {sum(time_list) / len(time_list):.4f} seconds")
+
 # Example usage
 def run_charge_cycle(ip: str, port: int):
     device = ITech6018Device(ip, port)
     controller = ChargeController(device, device)
     controller.add_state(PowerStates.CHARGE, current = 0.500, cutoff_voltage = 14.4, cutoff_current = 0.350)
     controller.add_state(PowerStates.DISCHARGE, current = -1.000, cutoff_voltage = 12.5, cutoff_current = -0.400)
+    controller.add_state(PowerStates.CHARGE, current = 0.500, cutoff_voltage = 14.4, cutoff_current = 0.350)
     controller.execute_sequence()
 
 run_charge_cycle('169.254.150.40', 30000)
-
+#benchmark_response_time('169.254.150.40', 30000)
 #simple_charge('169.254.150.40', 30000)
 #simple_discharge('169.254.150.40', 30000)
 
